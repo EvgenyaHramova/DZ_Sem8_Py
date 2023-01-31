cb830c7ad73fa7f36a7db7195ae5a4cabb339175
EvgenyaHramova <evgenya.hramova@yandex.ru>
2023-01-31 22:53:58 +0400

    file Zadacha1.py

--- Zadacha1.py
+++ Zadacha1.py
@@ -7,55 +7,30 @@ print(ingredients)  # [2, 2, 3]
 
 coffee_recipe = {'Эспрессо': [1, 0, 0], 'Капучино': [1, 3, 0], 'Маккиато': [2, 1, 0], 'Кофе по-венски': [1, 0, 2], 'Латте Маккиато': [1, 2, 1], 'Кон Панна': [1, 0, 1]}
 
-preference = input('Какие рецепты кофе предпочитаете? Назовите в порядке убывания предпочтений:  ').split(', ')
+preference = input('Какие рецепты кофе предпочитаете? Назовите в порядке убывания предпочтений:  ').split(',')
 print(preference)   # [Латте Маккиато, Маккиато, Капучино]
                     #    
-
+# def choose_coffee(preference):
 for i in preference:
     for key in coffee_recipe:
         if key == i:
             x = coffee_recipe.get(key)
-            if x[0] <= ingredients
-            for j in range(len(ingredients)):
-                while x[j] <= ingredients[j]:
-                    j+=1
+            ing = []
+            if x[0] <= ingredients[0] and x[1] <= ingredients[1] and x[2] <= ingredients[2]:
                 print(i)
-                ing = []
-                for k in range(len(ingredients)):  
                 
-                    if ingredients[k] < x[k]:
-                        ing.append(ingredients[k])
-                        k += 1
-                    else:
-                        ing.append(ingredients[k] - x[k])
-                        k += 1
+                for k in range(len(ingredients)):  
+                    ing.append(ingredients[k] - x[k])
+                
                 ingredients = ing
-                   
-            
-                print(ingredients)
-            print('Извините, нет достаточного количества ингредиентов для приготовления', key)
+                #print(ingredients)
+            else:
+                print('Извините, нет достаточного количества ингредиентов для приготовления', i)
+
             
 
             
 # print(choose_coffee)
 
-# def choose_coffee(preference):
-# for i in preference:
-    # for key in coffee_recipe:
-    #     if key == i:
-    #         x = coffee_recipe.get(key)
-    #         if ingredients < x:
-    #             print('Извините, нет достаточного количества ингредиентов для приготовления', key)
-    #         if ingredients >= x:    
-    #             ing = []
-    #             for k in range(0, len(ingredients)):
-    #                 if ingredients[k] >= x[k]:
-    #                     ing.append(ingredients[k] - x[k])
-                    
-    #                 else:
-    #                     ing.append(ingredients[k])
-    #                     k += 1
-    #             print(i)
-    #         ingredients = ing
-    #         print(ingredients)
 
+# 
